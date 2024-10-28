706d1c688c09714709cdc39924dadc4ae32667b0
JohanaLl <johisllanos@gmail.com>
2024-10-27 20:12:39 -0500

    Uso de multiples sentencias

--- FunctionaDev/src/functional/Main.java
+++ FunctionaDev/src/functional/Main.java
@@ -6,5 +6,15 @@ public class Main {
 		
 		ISaludar saludar = user -> System.out.println("Hola Mundo " + user); 
 		saludar.Saludar("Codi");
+		
+		ISaludar saludarDos = name -> {
+			
+			if (name.equals("Codi"))
+				System.out.println("Hola Codi! Tienes el mismo nombre que la mascota de CF");
+			else 
+				System.out.println("Hola Muindo " + name);
+		};
+		
+		saludarDos.Saludar("Codis");
 	}
 }
